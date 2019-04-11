a289326828a029ed2848f2921b10a5e821fadcaf
sung-eun <sungeunj0@naver.com>
2019-04-11 22:41:13 +0900

    계산기 기본 뷰 구성
    1. 히스토리 버튼
    2. 숫자 텍스트 박스
    3. 숫자, 연산 버튼들..

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -8,20 +8,45 @@ class Calculator extends StatefulWidget {
 class CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Calculator"),
+    final historyButton = IconButton(
+      icon: Icon(Icons.history),
+      onPressed: () {},
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator"),
+          actions: <Widget>[historyButton],
         ),
-        body: new GridView.count(
-          crossAxisCount: 4,
-          children: new List<Widget>.generate(20, (index) {
-            return new GridTile(
-              child: new Card(
-                  child: new Center(
-                    child: new Text('$index'),
-                  )),
-            );
-          }),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerRight,
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "0",
+                    style: TextStyle(fontSize: 50.0, color: Colors.black),
+                    textAlign: TextAlign.right,
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: GridView.count(
+                      crossAxisCount: 4,
+                      children: List<Widget>.generate(20, (index) {
+                        return GridTile(
+                          child: Card(
+                              child: Center(
+                            child: Text('$index'),
+                          )),
+                        );
+                      })))
+            ],
+          ),
         ));
   }
 }

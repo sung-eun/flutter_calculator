3c7bdef64dfd7553e17b61de7bbba8eedad29ceb
sung-eun <sungeunj0@naver.com>
2019-04-16 00:17:07 +0900

    계산기 뷰 완성, History 화면 추가
    TODOs
    - Calculator 계산 로직 구현
    - History 화면 구현

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,52 +1,5 @@
-import 'package:flutter/material.dart';
+class Calculator {
+  const Calculator();
 
-class Calculator extends StatefulWidget {
-  @override
-  State createState() => new CalculatorState();
-}
-
-class CalculatorState extends State<Calculator> {
-  @override
-  Widget build(BuildContext context) {
-    final historyButton = IconButton(
-      icon: Icon(Icons.history),
-      onPressed: () {},
-    );
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Calculator"),
-          actions: <Widget>[historyButton],
-        ),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.centerRight,
-                color: Colors.white,
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    "0",
-                    style: TextStyle(fontSize: 50.0, color: Colors.black),
-                    textAlign: TextAlign.right,
-                  ),
-                ),
-              ),
-              Expanded(
-                  child: GridView.count(
-                      crossAxisCount: 4,
-                      children: List<Widget>.generate(20, (index) {
-                        return GridTile(
-                          child: Card(
-                              child: Center(
-                            child: Text('$index'),
-                          )),
-                        );
-                      })))
-            ],
-          ),
-        ));
-  }
+  //TODO
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'calculator.dart';
+import 'history.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +19,121 @@ class MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.amber,
         ),
-        home: Calculator());
+        home: Main());
   }
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final historyButton = IconButton(
+      icon: Icon(Icons.history),
+      onPressed: () {
+        Navigator.push(context,
+            new MaterialPageRoute(builder: (context) => new History()));
+      },
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator"),
+          actions: <Widget>[historyButton],
+        ),
+        body: Column(
+          children: <Widget>[Label(), KeyPad()],
+        ));
+  }
+}
+
+class Label extends StatefulWidget {
+  @override
+  State<Label> createState() {
+    return LabelState();
+  }
+}
+
+class LabelState extends State<Label> {
+  var expression = "0";
+  var output = "";
+
+  @override
+  Widget build(BuildContext context) {
+    var expressionWidget = Text(
+      expression,
+      style: TextStyle(fontSize: 50.0, color: Colors.black),
+      textAlign: TextAlign.right,
+    );
+
+    var outputWidget = Text(
+      output,
+      style: TextStyle(fontSize: 50.0, color: Colors.black),
+      textAlign: TextAlign.right,
+    );
+
+    return Expanded(
+        flex: 2,
+        child: new Container(
+          alignment: Alignment.centerRight,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[expressionWidget, outputWidget],
+            ),
+          ),
+        ));
+  }
+}
+
+class KeyPad extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+        flex: 4,
+        child: new GridView.count(
+            crossAxisCount: 4,
+            padding: const EdgeInsets.all(1.5),
+            children: <String>[
+              'C', '%', 'x²', 'x³',
+              '7', '8', '9', '×',
+              '4', '5', '6', '−',
+              '1', '2', '3', '+',
+              '0', '.', 'x!', '='
+            ].map((key) {
+              return new GridTile(
+                child: new KeyPadButton(key),
+              );
+            }).toList(),
+          ),
+        );
+  }
+}
+
+class KeyPadButton extends StatelessWidget {
+  KeyPadButton(this.keyValue);
+
+  final keyValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return new OutlineButton(
+      child: new Text(
+        keyValue,
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22.0,
+          color: Colors.black,
+        ),
+      ),
+      borderSide: BorderSide(width: 0.5, color: Colors.black12),
+      onPressed: () {
+        onClickKeyPad(keyValue);
+      },
+    );
+  }
+}
+
+void onClickKeyPad(String key) {
+//TODO
 }
